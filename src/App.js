60dae7ae7fb75b345ffa45411f94cc4617119278
Andrew Bwogi <andrew.bwogi@gmail.com>
2022-10-15 14:02:38 -0700

    grid structure created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
 
 const App = () => {
 	return (
-		<div>
-			<h1>Hello</h1>
-		</div>
+		<>
+			{/* to reset the margins and padding use CssBaseline first */}
+			<CssBaseline />
+			<Header />
+			{/* here we have a grid of type container with a spacing of 3 and 100% width inline style */}
+			<Grid container spacing={3} style={{ width: '100%' }}>
+				{/* inside this we will have one more grid of type item and it will take all 12 spaces(full width) on mobile devices but on medium devices or larger, it will taks 4 spaces */}
+				<Grid item xs={12} md={4}>
+					<List />
+				</Grid>
+				{/* the map is going to take 8 spaces on medium or larger devices */}
+				<Grid item xs={12} md={8}>
+					<Map />
+				</Grid>
+			</Grid>
+		</>
 	);
 };
 
