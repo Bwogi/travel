d74a45e0344a3359c1306bbb3ff5cbe81f7df961
Andrew Bwogi <andrew.bwogi@gmail.com>
2022-10-15 19:09:11 -0700

    started work on the map but not complete

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core'; // paper is just a div with a background color and useMediaQuery will help with the map being mobile responsive
+import LocationOutlinedIcon from '@material-ui/icons/LocationOutlined';
+import Rating from '@material-ui/lab'; // Rating is a component within the material ui library. Its still in the lab and not in core
+import useStyles from './styles';
 
 const Map = () => {
-	return <div>Map</div>;
+	// here we call useStyles as a hook
+	const classes = useStyles();
+	const isMobile = useMediaQuery('(min-width:600px)');
+
+	const coordinates = { lat: 0, lng: 0 };
+	return (
+		<div className={classes.mapContainer}>
+			<GoogleMapReact
+				bootstrapURLKeys={{ key: '' }}
+				defaultCenter={coordinates}
+				center={coordinates}
+				deefaultZoom={14}
+				margin={[50, 50, 50, 50]}
+				options={''}
+				onChange={''}
+				onChildClick={''}
+			></GoogleMapReact>
+		</div>
+	);
 };
 
 export default Map;
