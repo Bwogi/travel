5831e62a0713989883abee4f979918a890ea2f52
Andrew Bwogi <andrew.bwogi@gmail.com>
2022-10-15 23:58:41 -0700

    map can now render

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 		<AppBar position='static'>
 			<Toolbar className={classes.toolbar}>
 				<Typography variant='h5' className={classes.title}>
-					Travel Advisor
+					Travel Snippets
 				</Typography>
 				<Box display='flex'>
 					<Typography variant='h6' className={classes.title}>
